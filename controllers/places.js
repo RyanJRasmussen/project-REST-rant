f46543afad81f59dc2c5f7becb0b3db540e003df
RyanJRasmussen <87948132+RyanJRasmussen@users.noreply.github.com>
2022-03-14 19:01:58 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,36 +1,30 @@
 const router = require('express').Router()
+let places = require('../models/places.js')
 
-
-// router.get('/new', (req, res) => {
-//     res.render('places/new')
-// })
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    console.log(places)
+    places.push(req.body)
+    res.redirect('/places')
 })
 
 // GET /places
 router.get('/', (req, res) => {
-    let places = [
-        {
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thaiPic.jpg'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/codingCat.jpg'
-    }
-]
-res.render('places/index', { places })
+    res.render('places/index', { places })
 })
 
 module.exports = router
-
-// Photo by <a href="https://unsplash.com/@amseaman?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Andrew Seaman</a> on <a href="https://unsplash.com/s/photos/thai-restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-// Photo by <a href="https://unsplash.com/@agforl24?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Tai Bui</a> on <a href="https://unsplash.com/s/photos/coding-cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
