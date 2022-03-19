42c5ff2c3bdebfe4c8394420ad843dc706028f9a
RyanJRasmussen <87948132+RyanJRasmussen@users.noreply.github.com>
2022-03-19 13:05:15 -0500

    Started adding mongoDB and mongoose ODM to interact with a real database, not just mock data

--- models/places.js
+++ models/places.js
@@ -1,16 +1,15 @@
-module.exports = [
-    {
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thaiPic.jpg'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/codingCat.jpg'
-}
-]
+const mongoose = require('mongoose')
+const placeSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    pic: String,
+    cuisines: {type: String, required: true},
+    city: {type: String, default: 'Anytown'}, 
+    state: {type: String, default: 'USA'},
+    founded: Number
+})
 
+module.exports = mongoose.model('Place', placeSchema)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
