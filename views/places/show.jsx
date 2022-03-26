069340abff0c4c018ba6efac2c6a6f6d45a65d89
RyanJRasmussen <87948132+RyanJRasmussen@users.noreply.github.com>
2022-03-26 14:27:39 -0500

    Got messages to add from the webpage

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -35,26 +35,50 @@ function show (data) {
                 <img src={data.place.pic} alt={data.place.name}></img>
                 <h3>{data.place.showEstablished()}</h3>
                 <h4>Serving {data.place.cuisines}</h4>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
                         <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                     </svg>
                     Edit
                 </a>
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
                     <button type="submit" className="btn btn-danger">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                            <path fillrule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                            <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                         </svg>
                         Delete
                     </button>
-                </form>    
+                </form>
+
+
                 <hr></hr>
                 <div id='rating'>Ratings</div>
                 <hr></hr>
                 <div className="h3">Comments</div>
                 <div className = "text-muted">{comments}</div>
+Add a comment!
+                <form method="POST" action={`/places/${data.place.id}/comment`}>
+                    <div className="form-group">
+                        <label htmlFor="author">Your name</label>
+                        <input className="form-control" id="author" name="author" required type={"text"}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="content">Comment content</label>
+                        <input className="form-control" id="content" name="content" type={"textarea"}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="stars">Star Rating</label>
+                        <input className="form-control" id="stars" name="stars" type="number" step="0.5" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="rant">Check for rant</label>
+                        <input className="" id="rant" name="rant" type="checkbox"/>
+                    </div>
+                    <input className="btn btn-primary" type="submit" value="Submit" />
+                </form>
+
+
                 <div className='footer'>Author: Ryan Rasmussen</div>
             </main>
         </Def>
