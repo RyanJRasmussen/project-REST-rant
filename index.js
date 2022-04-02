e896c8bab0ab44b49e38e1ccff83757825436768
RyanJRasmussen <87948132+RyanJRasmussen@users.noreply.github.com>
2022-04-02 10:44:40 -0500

    Changed port listening

--- index.js
+++ index.js
@@ -25,4 +25,6 @@ app.get('*', (req, res) => {
 })
 
 // Listen for Connections
-app.listen(process.env.PORT || 80)
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
