0938fb5f17d488351da623732c836dfbdf39fc33
RyanJRasmussen <87948132+RyanJRasmussen@users.noreply.github.com>
2022-04-02 10:39:57 -0500

    Added 80 lol

--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,7 @@ function home () {
             <main>
                 <h1>HOME</h1>
                 <div>
-                    <img src="/images/parrotPic.jpg" alt="A very cool parrot" width="500"/>
+                    <img src="./images/parrotPic.jpg" alt="A very cool parrot" width="500"/>
                     <div>
                     Photo by <a href="https://unsplash.com/@miklevasilyev?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mikhail Vasilyev</a> on <a href="https://unsplash.com/s/photos/free?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                     </div>
