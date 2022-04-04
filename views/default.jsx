cc2b3ebda176799a0f2b53a2fd9cdb569ac62de5
RyanJRasmussen <87948132+RyanJRasmussen@users.noreply.github.com>
2022-04-04 14:59:45 -0500

    Changed styles path yet again woohoo

--- views/default.jsx
+++ views/default.jsx
@@ -6,7 +6,7 @@ function Def (html) {
             <head>
                 <title>Title</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
-                <link rel="stylesheet" href="../public/CSS/style.css"></link>
+                <link rel="stylesheet" href="/public/CSS/style.css"></link>
             </head>
             <body>
             <nav>
